fc1511ebfdab8fb8be827ff741ddc49dfc7e0163
anecha.boon <anecha.boon@gmail.com>
2021-10-17 10:44:19 +0700

    create tab menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'FoodMenu.dart';
+
 void main() {
   var app = MyApp();
   runApp(app);
@@ -7,14 +8,13 @@ void main() {
 
 // create widget
 class MyApp extends StatelessWidget {
-  Widget build(BuildContext context) {
+Widget build(BuildContext context) {
     return MaterialApp(
       title: "My App",
       home: MyHomePage(),
       theme: ThemeData(primarySwatch: Colors.green),
     );
-  }
-}
+  }}
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -22,32 +22,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  List<FoodMenu> menu = [
-    FoodMenu("Shrimp","2000","assets/images/Shrimp.jpg"),
-    FoodMenu("Pork","100","assets/images/Pork.jpg"),
-  ];
-  int number = 0; // create state
-
   @override
   Widget build(BuildContext context) {
-    // display
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Harty"),
-        ),
-        body: ListView.builder(
-          itemCount: menu.length,
-          itemBuilder: (BuildContext context, int index) {
-            FoodMenu food = menu[index];
-            return ListTile(
-              leading: Image.asset(food.img),
-              title: Text(
-                "${food.name}",
-                style: TextStyle(fontSize: 25),
-              ),
-              subtitle: Text("price ${food.price}"),
-            );
-        }));
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          body: TabBarView(
+            children: [Container(), Container()],
+          ),
+          backgroundColor: Colors.lightBlueAccent,
+          bottomNavigationBar: TabBar(tabs: [
+            Tab(text: "บันทึกคะแนน",),
+            Tab(text: "รายชื่อนักเรียน",),
+          ]),
+        ));
   }
 }
